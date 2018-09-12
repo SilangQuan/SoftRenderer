b803aff6d1521e5308dd70ec8315635d90e569d7
= <=>
2018-09-12 16:22:57 +0800

    remove

--- Project/targetver.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// ���� SDKDDKVer.h ��������õ���߰汾�� Windows ƽ̨��
-
-// ���ҪΪ��ǰ�� Windows ƽ̨����Ӧ�ó�������� WinSDKVer.h������
-// �� _WIN32_WINNT ������ΪҪ֧�ֵ�ƽ̨��Ȼ���ٰ��� SDKDDKVer.h��
-
-//#include <SDKDDKVer.h>
